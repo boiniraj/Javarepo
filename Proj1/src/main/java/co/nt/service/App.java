97a3d986268034a89f8d4366758bf692eae091e4
rajkumar <boinirajkumar02@gmail.com>
2024-05-03 13:02:28 +0530

    TL-CreateProject

---
+++ Proj1/src/main/java/co/nt/service/App.java
@@ -0,0 +1,13 @@
+package co.nt.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
